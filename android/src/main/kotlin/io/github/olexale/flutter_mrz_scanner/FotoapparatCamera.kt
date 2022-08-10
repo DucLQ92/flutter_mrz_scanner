11084ba4412f1e8d89f9b445045eb64eafcba516
DucLQ92 <77601238+DucLQ92@users.noreply.github.com>
2022-08-10 15:47:04 +0700

    update trained data

--- android/src/main/kotlin/io/github/olexale/flutter_mrz_scanner/FotoapparatCamera.kt
+++ android/src/main/kotlin/io/github/olexale/flutter_mrz_scanner/FotoapparatCamera.kt
@@ -2,6 +2,7 @@ package io.github.olexale.flutter_mrz_scanner
 
 import android.content.Context
 import android.graphics.*
+import android.util.Log
 import androidx.annotation.NonNull
 import androidx.core.content.ContextCompat
 import com.googlecode.tesseract.android.TessBaseAPI
@@ -92,6 +93,7 @@ class FotoapparatCamera constructor(
         val bitmap = getImage(frame)
         val cropped = calculateCutoutRect(bitmap, true)
         val mrz = scanMRZ(cropped)
+        Log.e("///////////", mrz)
         val fixedMrz = extractMRZ(mrz)
         mainExecutor.execute {
             messenger.invokeMethod("onParsed", fixedMrz)
